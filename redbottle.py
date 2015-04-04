28ac079266da894e45913737a113fb483a74f18b
Jason Kolios <precursor@gmail.com>
2015-04-04 22:27:32 +0300

    Migrated to bottle-redis.

--- redbottle.py
+++ redbottle.py
@@ -19,7 +19,7 @@ def app_init():
 
 @bottledis_app.route('/')
 def show_home(db, session):
-    print session['user']
+    print type(session['user'])
     return bottle.template('home_template.tpl', {'session': session})
 
 
